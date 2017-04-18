9e3ab0c10c617399b4ae2b9c13748be762b4f7c2
AGagliano <andrea.gagliano@ischool.berkeley.edu>
2017-04-18 20:51:51 +0000

    microphone updates

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 
 from flask import render_template, Flask, jsonify, request
+import alsaaudio
 import sensor_data
+import microphone_level
 # I may not need the request here since I'm not getting user input
 app = Flask(__name__)
 
+#~ I may need to put this somewhere else..not sure if it executes without being called below
+@app.before_first_request
+def test_print():
+	print("ran this before starting website")
+
+
+#~ Get sound data from microphone
+@app.route('/_get_time')
+def get_time():
+    sensorData = sensor_data.run()
+    return jsonify(result=sensorData)
+
+#~ Get force and sound randomized data for testing
 @app.route('/_get_time')
 def get_time():
     sensorData = sensor_data.run()
