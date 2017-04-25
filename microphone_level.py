c2344525b1e864d690615a7d69692daa1be19218
AGagliano <andrea.gagliano@ischool.berkeley.edu>
2017-04-25 17:57:36 +0000

    microphone updates

--- microphone_level.py
+++ microphone_level.py
@@ -3,14 +3,18 @@
 import alsaaudio, time, audioop
 
 def setup():
+	print('in setup function')
 	card = 'sysdefault:CARD=Microphone'
+	print('changed card')
 	inp=alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
-
+	print('set inp, alsaasuio')
+	
 	inp.setchannels(1)
 	inp.setrate(8000)
 	inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 
 	inp.setperiodsize(160)
+	print('about to leave setup')
 	return inp
 
 #~ while True:
@@ -20,9 +24,15 @@ def setup():
 	#~ time.sleep(.001)
 
 def get_sound(inp):
+	#print('in get_sound function')
 	while True:
+		#print('in while loop')
 		l, data = inp.read()
 		if l>0:
 			val = audioop.max(data, 2)
 			return({'sound': val})
 		time.sleep(0.001)
+
+if __name__ == "__main__":
+	setup()
+	get_sound()
--- microphone_level_continuousRead.py
+++ microphone_level_continuousRead.py
@@ -12,6 +12,7 @@ inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 inp.setperiodsize(160)
 
 while True:
+	#print('in while')
 	l, data = inp.read()
 	if l:
 		print(audioop.max(data, 2))
